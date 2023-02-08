450b70322ec47c752ffe6b72903ddfa032783e9d
Daniel Matei <124709628+CSepsb@users.noreply.github.com>
2023-02-08 09:20:39 -0700

    completed

--- Complete-the phrase/main.js
+++ Complete-the phrase/main.js
@@ -1,6 +1,29 @@
-document.getElementById("blessing").addEventListener("click", order);
+document.getElementById("blessing").addEventListener("click", click1);
 
-function order();{ 
-    let blessingf =alert(`test`)
-    alert(blessing);
+function click1() {
+  alert("...disguise.");
+
+}
+document.getElementById("dime").addEventListener("click", click2);
+
+function click2() {
+  alert("...dozen.");
+
+}
+document.getElementById("late").addEventListener("click", click3);
+
+function click3() {
+  alert("...never.");
+
+}
+document.getElementById("pain").addEventListener("click", click4);
+
+function click4() {
+  alert("no gain.");
+}
+
+document.getElementById("time").addEventListener("click", click5);
+
+function click5() {
+  alert("when you're having fun.");
 }
