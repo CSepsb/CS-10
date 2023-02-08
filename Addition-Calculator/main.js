7da2b9bfcb2341d491002944f3a4235e08c05400
CSepsb <124709628+CSepsb@users.noreply.github.com>
2023-02-08 15:46:08 -0700

    working

--- Addition-Calculator/main.js
+++ Addition-Calculator/main.js
@@ -1,2 +1,15 @@
-//Program TItle
-console.log(`hi`);
+// Simple addition calculator
+
+// Btn event listiner
+document.getElementById("btn").addEventListener("click", btnclick);
+
+function btnclick(){
+    // Input
+    let num1v = +document.getElementById("num1").value;
+    let num2v = +document.getElementById("num2").value;
+
+    // Process
+    let total=num1v+num2v;
+    // Output
+    document.getElementById("output").innerHTML=total
+}
