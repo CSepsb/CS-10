d19f613011f7470c02ecbf842037d54a98c0c7ca
Daniel Matei <124709628+CSepsb@users.noreply.github.com>
2023-02-08 09:55:56 -0700

    completed

--- Mad-Lib/main.js
+++ Mad-Lib/main.js
@@ -1,2 +1,9 @@
-//Program TItle
-console.log(`hi`);
+document.getElementById("bnt").addEventListener("click", Madlib);
+function Madlib() {
+  let member = document.getElementById("member").value;
+  let noun = document.getElementById("noun").value;
+  let action = document.getElementById("action").value;
+
+  let message = `My ${member} loves to eat ${noun} and then ${action}.`;
+  document.getElementById("output").innerHTML = message;
+}
