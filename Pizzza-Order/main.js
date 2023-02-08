800fdb219a61ca36b20b3b300ed114dbe2939694
Daniel Matei <124709628+CSepsb@users.noreply.github.com>
2023-02-08 09:36:38 -0700

    done

--- Pizzza-Order/main.js
+++ Pizzza-Order/main.js
@@ -5,11 +5,11 @@ document.getElementById("button").addEventListener("click", order);
 //Stor order code in a dunction
 function order() {
   // Input
-  let size = prompt(`Enter size of pizza (small, medium, large)`);
-  let topping1 = prompt(`Enter first topping`);
-  let topping2 = prompt(`Enter first topping`);
+  let size = document.getElementById("size").value;
+  let topping1 = document.getElementById("top1").value;
+  let topping2 = document.getElementById("top2").value;
   // Process
   let message = `Your ${size} pizza with ${topping1} and ${topping2} will be ready soon!`;
   // Output
-  alert(message);
+  document.getElementById("output").innerHTML = message;
 }
