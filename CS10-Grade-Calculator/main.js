e2579eb766c4bb333fa27898083db672b8a0fc7d
CSepsb <124709628+CSepsb@users.noreply.github.com>
2023-02-10 08:57:22 -0700

    done

--- CS10-Grade-Calculator/main.js
+++ CS10-Grade-Calculator/main.js
@@ -1,16 +1,17 @@
 // Grade calculator
 document.getElementById("button").addEventListener("click", bntclick);
 function bntclick() {
-//Input
+  //Input
   let script1 = +document.getElementById("scri1").value;
   let programming1 = +document.getElementById("pro1").value;
   let programming2 = +document.getElementById("pro2").value;
   let script2 = +document.getElementById("scri2").value;
   let projectA = +document.getElementById("proj").value;
-//Process
+  
+  //Process
   let percentage =
     (script1 + programming1 + programming2 + script2 + projectA) / 5.0;
-  document.getElementById("output").innerHTML =
-//Output 
-    Math.round((percentage + Number.EPSILON) * 100) / 100;
+  
+    //Output
+    document.getElementById("output").innerHTML = Math.round((percentage + Number.EPSILON) * 100) / 100;
 }
