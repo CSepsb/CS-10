f14756d5e53daaa15fc2571bd67de62dc0b08def
CSepsb <124709628+CSepsb@users.noreply.github.com>
2023-02-10 08:44:22 -0700

    done

--- Mad-Lib/main.js
+++ Mad-Lib/main.js
@@ -1,9 +1,12 @@
+// Mad Lib Project
 document.getElementById("bnt").addEventListener("click", Madlib);
 function Madlib() {
+  // Input
   let member = document.getElementById("member").value;
   let noun = document.getElementById("noun").value;
   let action = document.getElementById("action").value;
-
+// Process
   let message = `My ${member} loves to eat ${noun} and then ${action}.`;
+// Output
   document.getElementById("output").innerHTML = message;
 }
