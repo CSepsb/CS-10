a6eee86ed5a42adadeeba4f637826689d4eed420
CSepsb <124709628+CSepsb@users.noreply.github.com>
2023-02-14 09:53:16 -0700

    done

---
+++ Password-Checker/main.js
@@ -0,0 +1,18 @@
+// Passcode Checker
+
+// Button Event Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  // Inputs
+  let code1 = +document.getElementById("code1-in").value;
+  let code2 = +document.getElementById("code2-in").value;
+  let code3 = +document.getElementById("code3-in").value;
+
+  // Check Passcode
+  if (code1 === 10 && (code2 === 20 || code3 === 30)) {
+    document.getElementById("status").style.backgroundColor = "green";
+  } else {
+    document.getElementById("status").style.backgroundColor = "red";
+  }
+}
