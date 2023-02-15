e0eb3d127d9c8e9da90fd96913915d36e8c1326a
Daniel Matei <d.matei@EDU.epsb.ca>
2023-02-15 10:00:19 -0700

    done

--- Acronym-Lookup/main.js
+++ Acronym-Lookup/main.js
@@ -1,2 +1,11 @@
-//Program TItle
-console.log(`hi`);
+document.getElementById("search-btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  //   Get search input
+  let name = document.getElementById("search-in").value.toLowerCase();
+  console.log(name);
+  // Output
+  if (name === "fomo") {
+    document.getElementById("tester").innerHTML = `Test`;
+  }
+}
