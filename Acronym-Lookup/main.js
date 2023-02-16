11db0be734eb517cb925d448136ce0ffcb27d6a5
Daniel Matei <d.matei@EDU.epsb.ca>
2023-02-16 09:05:13 -0700

    done

--- Acronym-Lookup/main.js
+++ Acronym-Lookup/main.js
@@ -1,11 +1,29 @@
+// Acronym Lookup
+
 document.getElementById("search-btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
   //   Get search input
   let name = document.getElementById("search-in").value.toLowerCase();
   console.log(name);
-  // Output
+  // Process & Output
   if (name === "fomo") {
-    document.getElementById("tester").innerHTML = `Test`;
+    document.getElementById("output-text").innerHTML = `Fear of Missing Out`;
+  } else if (name === "html") {
+    document.getElementById(
+      "output-text"
+    ).innerHTML = `HyperText Markup Language`;
+  } else if (name === "css") {
+    document.getElementById("output-text").innerHTML = `Cascading Style Sheet`;
+  } else if (name === "gui") {
+    document.getElementById(
+      "output-text"
+    ).innerHTML = `Graphical User Interface`;
+  } else if (name === "ftp") {
+    document.getElementById("output-text").innerHTML = `File Transfer Program`;
+  } else {
+    document.getElementById(
+      "output-text"
+    ).innerHTML = `I don't know that acronym.`;
   }
 }
