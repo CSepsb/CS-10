67623b96a74c04942234a7b9d95417ceb228e184
Daniel Matei <d.matei@EDU.epsb.ca>
2023-04-19 09:05:02 -0600

    done

--- Mad-Lib/main.js
+++ Mad-Lib/main.js
@@ -1,12 +1,12 @@
 // Mad Lib Project
-document.getElementById("bnt").addEventListener("click", Madlib);
+document.getElementById("btn").addEventListener("click", Madlib);
 function Madlib() {
   // Input
   let member = document.getElementById("member").value;
   let noun = document.getElementById("noun").value;
   let action = document.getElementById("action").value;
-// Process
+  // Process
   let message = `My ${member} loves to eat ${noun} and then ${action}.`;
-// Output
-  dcument.getElementByIod("output").innerHTML = message;
+  // Output
+  document.getElementById("output").innerHTML = message;
 }
