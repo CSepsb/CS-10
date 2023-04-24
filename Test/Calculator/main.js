fbd52316fef9a917cc8165f0cea403916d2d0cb4
Daniel Matei <d.matei@EDU.epsb.ca>
2023-04-24 08:53:34 -0600

    done

--- Test/Calculator/main.js
+++ Test/Calculator/main.js
@@ -3,8 +3,8 @@ document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
   // Input
-  let mass = document.getElementById("mass-in").value;
-  let speed = document.getElementById("speed-in").value;
+  let mass = +document.getElementById("mass-in").value;
+  let speed = +document.getElementById("speed-in").value;
 
   // Procces
   let answer = (mass * speed ** 2) / 2;
