5fb679b0053af304e3656164cad5c28a6c5e5bfa
Daniel Matei <d.matei@EDU.epsb.ca>
2023-05-29 09:57:33 -0600

    done

--- Wall-Collide/main.js
+++ Wall-Collide/main.js
@@ -13,25 +13,51 @@ document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function keyDownHandler(event) {
-  if (event.keyCode === 39) {
+  if (event.keyCode === 68) {
     rightPressed = true;
-  } else if (event.keyCode === 37) {
+  } else if (event.keyCode === 65) {
     leftPressed = true;
   }
-  if (event.keyCode === 40) {
+  if (event.keyCode === 83) {
     downPressed = true;
-  } else if (event.keyCode === 38) {
+  } else if (event.keyCode === 87) {
     upPressed = true;
   }
 }
 
 function keyUpHandler(event) {
-  rightPressed = false;
-  leftPressed = false;
-  downPressed = false;
-  upPressed = false;
+  if (event.keyCode === 68) {
+    rightPressed = false;
+  } else if (event.keyCode === 65) {
+    leftPressed = false;
+  }
+  if (event.keyCode === 83) {
+    downPressed = false;
+  } else if (event.keyCode === 87) {
+    upPressed = false;
+  }
 }
 
+// Move
+function move() {
+  if (rightPressed === true) {
+    playerX += 3;
+  } else if (leftPressed === true) {
+    playerX -= 3;
+  }
+
+  if (downPressed === true) {
+    playerY += 3;
+  } else if (upPressed === true) {
+    playerY -= 3;
+  }
+}
+
+// Boundaries
+if (playerX >= 700 || playerY >= 700) {
+  playerX = 700;
+  playerY = 700;
+}
 requestAnimationFrame(draw);
 function draw() {
   // Canvas
@@ -40,6 +66,8 @@ function draw() {
   cnv.width = 700;
   cnv.height = 700;
 
+  //Move
+  move();
   // Wall
   ctx.fillStyle = "grey";
   ctx.fillRect(325, 250, 50, 250);
@@ -47,6 +75,5 @@ function draw() {
   ctx.fillStyle = "blue";
   ctx.fillRect(playerX, playerY, 50, 50);
 
-  // REQUEST ANIMATION FRAME
   requestAnimationFrame(draw);
 }
